645369d907d75c1111bab5c64091fb5a9e4b1f75
Erycsmart <ericsmart276@gmail.com>
2026-08-13 21:55:58 +0300

    prepare admin server for deployment

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,9 +1,28 @@
+import "dotenv/config";
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ?.replace(/\\n/g, "\n")
+};
+
+if (!serviceAccount.projectId ||
+    !serviceAccount.clientEmail ||
+    !serviceAccount.privateKey) {
+
+    throw new Error(
+        "Firebase Admin environment variables are missing."
+    );
+}
 
 admin.initializeApp({
+
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://nansubuga-869c6-default-rtdb.firebaseio.com"
+
+    databaseURL:
+        "https://nansubuga-869c6-default-rtdb.firebaseio.com"
+
 });
 
 export const auth = admin.auth();
